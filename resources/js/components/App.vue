bfaf5e012dde98740d55273b3af4e73e129cc2fb
FilipLendel <filiplendel02@gmail.com>
2024-12-28 12:42:41 +0100

    Initial commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1 className=" text-4xl tracking-tighter font-semibold">Wolt by Szympek & Fifi</h1>
+        <p>Udało się coś tam zrobić</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
